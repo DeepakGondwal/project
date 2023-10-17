711788d20ca939cbdad768094a6096120b10a656
deepak <145280014+DeepakGondwal@users.noreply.github.com>
2023-10-17 10:06:48 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,63 @@
+<section class="container">
+        <div class="row ">
+            <!-- bloglist -->
+            <div class="col-12">
+            <div class="col-8 mb-5">
+                <div class="card">
+                    <img src="img/single_blog_1.png.webp" alt="Product Image" style="width: 100%">
+                    <div class="container">
+                        <a href=""><b>Google inks pact for new 35-storey office</b></a>
+                        <p>That dominion stars lights dominion divide years for fourth have don't stars is that he earth
+                            it first without heaven in place seed it second morning saying.</p>
+                        <p class="card-link"><i class="bi bi-person"></i>Travel,Lifestyle | <i
+                                class="bi bi-chat-right-text"></i> 03 Comments </p>
+                    </div>
+                </div>
+            </div>
+                <!-- Repeat the card s -->
+                <div class="col-8 mb-5">
+                    <div class="card">
+                        <img src="img/single_blog_2.png.webp" alt="Product Image" style="width: 100%">
+                        <div class="container">
+                            <a href=""><b>Google inks pact for new 35-storey office</b></a>
+                            <p>That dominion stars lights dominion divide years for fourth have don't stars is that he
+                                earth it first without heaven in place seed it second morning saying.</p>
+                            <p class="card-link"><i class="bi bi-person"></i>Travel,Lifestyle | <i
+                                    class="bi bi-chat-right-text"></i> 03 Comments </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Repeat the card -->
+                <div class="col-8 mb-5">
+                    <div class="card">
+                        <img src="img/single_blog_3.png.webp" alt="Product Image" style="width: 100%">
+                        <div class="container">
+                            <a href=""><b>Google inks pact for new 35-storey office</b></a>
+                            <p>That dominion stars lights dominion divide years for fourth have don't stars is that he
+                                earth it first without heaven in place seed it second morning saying.</p>
+                            <p class="card-link"><i class="bi bi-person"></i>Travel,Lifestyle | <i
+                                    class="bi bi-chat-right-text"></i> 03 Comments </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Repeat the card-->
+                <div class="col-8 mb-5">
+                    <div class="card">
+                        <img src="img/single_blog_5.png.webp" alt="Product Image" style="width: 100%">
+                        <div class="container">
+                            <a href=""><b>Google inks pact for new 35-storey office</b></a>
+                            <p>That dominion stars lights dominion divide years for fourth have don't stars is that he
+                                earth it first without heaven in place seed it second morning saying.</p>
+                            <p class="card-link"><i class="bi bi-person"></i>Travel,Lifestyle | <i
+                                    class="bi bi-chat-right-text"></i> 03 Comments </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Repeat the card  -->
+                <button class="btn btn views_btn">Load More</button>
+                               
+            </div>
+         </div>
+    </div>
+    </section>
